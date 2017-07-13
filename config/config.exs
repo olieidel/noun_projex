4f26bcbe708ab06a2669d4d14b690b8d6928ec8f
Oliver Eidel <oli@eidel.net>
2017-07-13 14:26:24 +0200

    fix reading dev.secret.exs in config

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,9 @@
 use Mix.Config
 
+dev_secret_path = Path.expand("config/dev.secret.exs")
+
 if Mix.env in [:dev, :test] do
-  if File.exists?("dev.secret.exs") do
+  if File.exists?(dev_secret_path) do
     import_config "dev.secret.exs"
   end
 end
